0ea3d70df9d33b8b8bf8cd08aebb91e77e4e32cb
zeflow <florizs@gmail.com>
2017-01-03 11:28:43 +0100

    updated version from Maarten

--- print.c
+++ print.c
@@ -18,15 +18,28 @@ rdtsc()
 volatile int i;
 
 void
-test()
+test(int *n, float *avrg, int *min, int *max, FILE *fp)
 {
     uint64_t start, end;
     volatile int j;
+    
 
     start = rdtsc();
     j = i;
     end = rdtsc();
-    printf("took %lu ticks\n", end - start);
+    if((end-start)>0){
+	(*avrg)=(float)(((end-start)+(*avrg)*(*n))/((*n)+1));
+	if((end-start)<(*min)){
+	    (*min)=end-start;
+	}
+	if((end-start)>(*max)){
+	    (*max)=(end-start);
+	}
+    (*n)++;
+
+    fprintf(fp,"%lu\n",(end-start));
+
+    }
 }
 
 int
@@ -39,6 +52,21 @@ main(int ac, char **av)
     test();
     test();
 	*/
-	for( ; ;){test();}
+    int n=0;
+    int min=100;
+    int max=0;
+    float avrg=0;
+    FILE * fp;
+    fp = fopen ("file.txt", "w");
+
+
+
+
+   
+
+	for( ; ;){test(&n,&avrg,&min,&max,fp);}
+
+    fclose(fp);
+
     return 0;
 }
